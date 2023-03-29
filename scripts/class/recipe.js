8aad462ac928491dfce0fafd824afcfe5900ccb5
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2023-03-29 16:06:01 +0200

    added search by tags

--- scripts/class/recipe.js
+++ scripts/class/recipe.js
@@ -21,7 +21,7 @@ class Recipe {
                 if(searchResult.length > 0){
                     Recipe.displayRecipes(searchResult)
                     const tags = Utils.getTags(searchResult)
-                    Dropdown.fillDropwdowns(tags)
+                    Dropdown.fillDropDowns(tags)
                     Dropdown.handleTagsSearch(tags)
                     Dropdown.handleSelectedTags(tags)
                 }
@@ -46,15 +46,12 @@ class Recipe {
         
         
         for (let i = 0; i < recipes.length; i++) {
-            let ingredients = recipes[i].ingredients.map(function(ing){
-                return ing.ingredient
-            })
+            let ingredients = recipes[i].ingredients.map(ing => ing.ingredient.toLowerCase());
 
-            if(recipes[i].name.includes(searchTerm) || recipes[i].description.includes(searchTerm) || ingredients.includes(searchTerm) ) {
+            if(recipes[i].name.toLowerCase().includes(searchTerm) || recipes[i].description.toLowerCase().includes(searchTerm) || ingredients.includes(searchTerm) ) {
                 filteredRecipes.push(recipes[i])
             }
         }
-
         return filteredRecipes
     }
 
@@ -63,25 +60,39 @@ class Recipe {
         const tagBadges = document.querySelectorAll('.filter__tag--text') 
         const selectedBadges = [];
         
-        tagBadges.forEach(badge => {
-            badge = badge.textContent
-            selectedBadges.push(badge)
-        })
+        for (let i = 0; i < tagBadges.length; i++){
+            selectedBadges.push(tagBadges[i].textContent);
+        }
 
-        const filteredBadges = recipes.filter(recipe => {
+        const filteredRecipesByTag = [];
+
+        for (let i = 0; i < recipes.length; i++) {
+            const recipe = recipes[i];
+
             const matchingIngredients = recipe.ingredients.filter(ingredient => {
-                return selectedBadges.some(tag => ingredient.ingredient.toLowerCase().includes(tag.toLowerCase()))
-            });
+                for (let ing = 0; ing < selectedBadges.length; ing++) {
+                    if(ingredient.ingredient.toLowerCase().includes(selectedBadges[ing].toLowerCase())){
+                        return true;
+                    }
+                }
+            })
 
-            const matchingAppliances =  selectedBadges.includes(recipe.appliance)
+            const matchingAppliances = selectedBadges.includes(recipe.appliance.toLowerCase());
 
-            const matchingUstensils = selectedBadges.some(badge => recipe.ustensils.includes(badge.toLowerCase()))
+            const matchingUstensils = recipe.ustensils.filter(ustensil => {
+                for (let ust = 0; ust < selectedBadges.length; ust++) {
+                    if (ustensil.toLowerCase().includes(selectedBadges[ust].toLowerCase())) {
+                        return true;
+                    }
+                }
+                return false;
+            }).length > 0;
 
-            return  matchingIngredients.length > 0 ||
-                     matchingAppliances ||
-                     matchingUstensils;
-        })
-        Recipe.displayRecipes(filteredBadges)
+            if (matchingIngredients.length > 0 || matchingAppliances || matchingUstensils ) {
+                filteredRecipesByTag.push(recipe);
+            }
+        }
+        Recipe.displayRecipes(filteredRecipesByTag)
         
     }
 
